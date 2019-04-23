8fb44be187b44fd85b273d80f96c5083915fcdd5
Masato Hagiwara <hagisan@gmail.com>
2019-04-22 21:55:15 -0400

    Add more validation

--- validate.py
+++ validate.py
@@ -3,19 +3,37 @@ import yaml
 
 MANDATORY_FIELDS = ['word', 'romaja', 'pos', 'senses']
 OPTIONAL_FIELDS = ['index', 'hanja', 'conj', 'notes', 'tags']
+POS = ['n', 'v', 'a', 'pron', 'intj', 'det']
+TAGS = ['topik1']
 
 
 def validate_entry(entry):
+    # TODO: Use JSON Schema instead to validate
+
     if not isinstance(entry, dict):
         raise ValueError('Entry {!r} is not a dict'.format(entry))
 
     for field in MANDATORY_FIELDS:
-        if  field not in entry:
+        if field not in entry:
             raise ValueError('Entry {!r} does not have the mandatory `{}` field'.format(entry, field))
 
     unrecognized_fields = set(entry.keys()) - set(MANDATORY_FIELDS) - set(OPTIONAL_FIELDS)
     if unrecognized_fields:
         raise ValueError('Entry {!r} has unrecognized field(s): {!r}'.format(entry, unrecognized_fields))
+
+    if entry['pos'] not in POS:
+        raise ValueError('Unrecognized POS tag: {}'.format(entry['pos']))
+
+    if 'conj' in entry:
+        if not isinstance(entry['conj'], list):
+            raise ValueError('Conjugation {!r} is not a list'.format(entry['conj']))
+
+    if 'tags' in entry:
+        if not isinstance(entry['tags'], list):
+            raise ValueError('Tags {!r} is not a list'.format(entry['tags']))
+        for tag in entry['tags']:
+            if tag not in TAGS:
+                raise ValueError('Unrecognized tag: {}'.format(tag))
 
 
 def validate_dictionary(data):
